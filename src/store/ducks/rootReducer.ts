6ffa523973731ef265701d97cbbb3ab6572f50a6
Weslleyzada <weslleynfs@gmail.com>
2020-03-17 14:45:25 -0400

    Inicio de implementacao de aplicacao em React com TypeScript

---
+++ src/store/ducks/rootReducer.ts
@@ -0,0 +1,12 @@
+/* eslint-disable import/extensions */
+/* eslint-disable comma-dangle */
+/* eslint-disable indent */
+/* eslint-disable eol-last */
+/* eslint-disable semi */
+import { combineReducers } from 'redux'
+
+import repositories from './repositories'
+
+export default combineReducers({
+    repositories
+})
